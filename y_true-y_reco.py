832c2f888980294bb47f65bdd9421c1031e556e6
OrghoN <anoronyo@gmail.com>
2017-05-18 11:37:26 -0500

    chanding back to simple draw function

--- y_true-y_reco.py
+++ y_true-y_reco.py
@@ -5,8 +5,9 @@ from ROOT import *
 # c1 = TCanvas( 'yComparison', 'yComparison', 200, 10, 700, 500 )
 
 f = TFile.Open("ndtf_output_nubar_gar.dst.root")
-tree = f.Get("dune_dst")
-tree.Draw("y_reco:y_true")
+# tree = f.Get("dune_dst")
+# tree.Draw("y_reco:y_true")
+tree.Draw("Y_reco")
 
 # for event in f.dune_dst:
     # print event.y_reco
