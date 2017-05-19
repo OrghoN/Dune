a85375fd70d75050514a97f746bdb33b0572ab13
Orgho Neogi <oneogi@dunegpvm09.fnal.gov>
2017-05-18 19:50:44 -0500

    changed path to dune machine

--- y_true-y_reco.py
+++ y_true-y_reco.py
@@ -4,12 +4,12 @@ from ROOT import *
 # gSystem.Load("dunendtf")
 # c1 = TCanvas( 'yComparison', 'yComparison', 200, 10, 700, 500 )
 
-f = TFile.Open("ndtf_output_nubar_gar.dst.root")
-# tree = f.Get("dune_dst")
-# tree.Draw("y_reco:y_true")
-tree.Draw("Y_reco")
+f = TFile.Open("/pnfs/dune/persistent/TaskForce_AnaTree/ndtf_output/4th_run/ndtf_output_nubar_gar.dst.root")
+tree = f.Get("dune_dst")
+tree.Draw("y_reco:y_true")
+#tree.Draw("Y_reco")
 
-# for event in f.dune_dst:
-    # print event.y_reco
+#for event in f.dune_dst:
+#    print event.y_reco
 
-# gApplication.Run()
+gApplication.Run()
