4edbfe7116102210b748494992ddeacc3ccf6d64
OrghoN <anoronyo@gmail.com>
2017-06-23 08:48:45 -0500

    added 3 vertex position

--- positionResolution.C
+++ positionResolution.C
@@ -3,10 +3,16 @@ void positionResolution(){
 // open files
         vector<TFile *> files {new TFile("/pnfs/dune/persistent/TaskForce_AnaTree/ndtf_output/4th_run/ndtf_output_nubar_fgt.dst.root","READ"),new TFile("/pnfs/dune/persistent/TaskForce_AnaTree/ndtf_output/4th_run/ndtf_output_nu_fgt.dst.root","READ"),new TFile("/pnfs/dune/persistent/TaskForce_AnaTree/ndtf_output/4th_run/ndtf_output_nubar_gar.dst.root","READ"),new TFile("/pnfs/dune/persistent/TaskForce_AnaTree/ndtf_output/4th_run/ndtf_output_nu_gar.dst.root","READ")};
 
-        vector<TH1F *> difference {new TH1F("nubar_fgt_dif","y_true-y_reco",100,0,1), new TH1F("nu_fgt_dif","y_true-y_reco",100,0,1), new TH1F("nubar_gar_dif","y_true-y_reco",100,0,1), new TH1F("nu_gar_dif","y_true-y_reco",100,0,1)};
-        vector<TH1F *> division {new TH1F("nubar_fgt_div","y_true-y_reco",100,0,1), new TH1F("nu_fgt_div","y_true-y_reco",100,0,1), new TH1F("nubar_gar_div","y_true-y_reco",100,0,1), new TH1F("nu_gar_div","y_true-y_reco",100,0,1)};
-        vector<TH1F *> h_true {new TH1F("nubar_fgt_y_true","y_true",100,0,1), new TH1F("nu_fgt_y_true","y_true",100,0,1), new TH1F("nubar_gar_y_true","y_true",100,0,1), new TH1F("nu_gar_y_true","y_true",100,0,1)};
-        vector<TH1F *> h_reco {new TH1F("nubar_fgt_y_reco","y_reco",100,0,1), new TH1F("nu_fgt_y_reco","y_reco",100,0,1), new TH1F("nubar_gar_y_reco","y_reco",100,0,1), new TH1F("nu_gar_y_reco","y_reco",100,0,1)};
+        vector<TH1F *> differenceX {new TH1F("nubar_fgt_difX","y_true-y_reco",100,0,1), new TH1F("nu_fgt_dif","y_true-y_reco",100,0,1), new TH1F("nubar_gar_dif","y_true-y_reco",100,0,1), new TH1F("nu_gar_dif","y_true-y_reco",100,0,1)};
+        vector<TH1F *> divisionX {new TH1F("nubar_fgt_divX","y_true-y_reco",100,0,1), new TH1F("nu_fgt_div","y_true-y_reco",100,0,1), new TH1F("nubar_gar_div","y_true-y_reco",100,0,1), new TH1F("nu_gar_div","y_true-y_reco",100,0,1)};
+        vector<TH1F *> differenceY {new TH1F("nubar_fgt_difY","y_true-y_reco",100,0,1), new TH1F("nu_fgt_dif","y_true-y_reco",100,0,1), new TH1F("nubar_gar_dif","y_true-y_reco",100,0,1), new TH1F("nu_gar_dif","y_true-y_reco",100,0,1)};
+        vector<TH1F *> divisionY {new TH1F("nubar_fgt_divY","y_true-y_reco",100,0,1), new TH1F("nu_fgt_div","y_true-y_reco",100,0,1), new TH1F("nubar_gar_div","y_true-y_reco",100,0,1), new TH1F("nu_gar_div","y_true-y_reco",100,0,1)};
+        vector<TH1F *> differenceZ {new TH1F("nubar_fgt_difZ","y_true-y_reco",100,0,1), new TH1F("nu_fgt_dif","y_true-y_reco",100,0,1), new TH1F("nubar_gar_dif","y_true-y_reco",100,0,1), new TH1F("nu_gar_dif","y_true-y_reco",100,0,1)};
+        vector<TH1F *> divisionZ {new TH1F("nubar_fgt_divZ","y_true-y_reco",100,0,1), new TH1F("nu_fgt_div","y_true-y_reco",100,0,1), new TH1F("nubar_gar_div","y_true-y_reco",100,0,1), new TH1F("nu_gar_div","y_true-y_reco",100,0,1)};
+
+        vector<TH1F *> differenceE {new TH1F("nubar_fgt_difE","y_true-y_reco",100,0,1), new TH1F("nu_fgt_dif","y_true-y_reco",100,0,1), new TH1F("nubar_gar_dif","y_true-y_reco",100,0,1), new TH1F("nu_gar_dif","y_true-y_reco",100,0,1)};
+        vector<TH1F *> divisionE {new TH1F("nubar_fgt_divE","y_true-y_reco",100,0,1), new TH1F("nu_fgt_div","y_true-y_reco",100,0,1), new TH1F("nubar_gar_div","y_true-y_reco",100,0,1), new TH1F("nu_gar_div","y_true-y_reco",100,0,1)};
+
 
         TFile *output = new TFile("/dune/data/users/oneogi/positionResolution.root","NEW");
 
